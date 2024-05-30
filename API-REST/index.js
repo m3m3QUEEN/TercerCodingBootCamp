5f36d929c73c0d96b64fcc5444a1aa48ed3e8a3e
m3m3QUEEN <a.zapata4@utp.edu.co>
2024-05-29 20:40:06 -0500

    endpoint pokemon

--- API-REST/index.js
+++ API-REST/index.js
@@ -1,19 +1,21 @@
 // index.js
 
 import express from "express";
-import connection from "./DBConnection.js"; // Asegúrate de ajustar la ruta de acuerdo a la ubicación de tu archivo de conexión
+import connection from "./DBConnection.js"; // Ajustado para que sea relativo a index.js
+
 const app = express();
 const port = 3000;
 
-// const userRouter = require("./src/routes/usersRoutes.js");
-// Usar el router de usuarios
+// Importar rutas usando rutas relativas correctas
 import userRoutes from "./src/routes/usersRoutes.js";
 import typesRoutes from "./src/routes/typesRoutes.js";
+import pokemonsRoutes from "./src/routes/pokemonsRoutes.js"; // Corregido, debería tener el prefijo './'
 
 app.use(express.json()); // Para parsear JSON en las peticiones
 
 app.use("/users", userRoutes);
 app.use("/types", typesRoutes);
+app.use("/pokemons", pokemonsRoutes);
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
