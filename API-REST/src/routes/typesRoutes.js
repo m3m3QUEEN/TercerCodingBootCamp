d5189c71bb52f0d048206fae9976c3cacbb5d899
unknown <santiago.castano@utp.edu.co>
2024-05-30 02:41:06 -0500

    crud

--- API-REST/src/routes/typesRoutes.js
+++ API-REST/src/routes/typesRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 
-import { createType, getAllTypes } from "../controllers/typesController.js";
+import { createType, getAllTypes, deleteType, updateType } from "../controllers/typesController.js";
 
 const router = express.Router();
 
 router.post("/", createType);
 router.get("/", getAllTypes);
+router.delete("/:id", deleteType)
+router.put("/:id", updateType )
 
 export default router;
